97277a10df08f9d27f026dba8615ca499f4f2c3c
Ayomide-Abim <ayomideadeleke@outlook.com>
2025-07-25 22:38:48 +0100

    Team Members sections and styling

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.index, name='index'),
-    path('about', views.index, name='about'),
+    path('home', views.index, name='home'),
+    path('about', views.about, name='about'),
     path('contact', views.index, name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,3 +6,5 @@ from . import views
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+def about(request):
+    return render(request, 'core/index.html')
