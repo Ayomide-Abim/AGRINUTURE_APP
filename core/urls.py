d3ae31222e78f2fc1e55d45dfbfa160e5efe632e
Ayomide-Abim <ayomideadeleke@outlook.com>
2025-08-05 19:11:28 +0100

    Some update

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from .views import contact_view
 urlpatterns = [
     path('home', views.index, name='home'),
     path('about', views.about, name='about'),
-    path('contact/', contact_view, name='contact'),
+    path('contact/', views.contact, name='contact'),
     path('services/', views.services_view, name='services'),  # ✅ Add this line
-    path('contact/submit/', contact_view, name='contact_submit'),
+    path('contact/submit/', views.contact_view, name='contact_submit'),
 ]
